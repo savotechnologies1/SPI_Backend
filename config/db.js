162ec01682dd35e7871b47267176643a2c2e39bd
savotechnologies1 <git.stpl@gmail.com>
2025-06-05 16:32:48 +0530

    updated cdoe

--- config/db.js
+++ config/db.js
@@ -3,10 +3,10 @@ const dotenv = require('dotenv');
 
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "SHI@2002",
+  database: process.env.DB_NAME || 'SPI',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
