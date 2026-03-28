96d8ebe1f7e113a59295e2b7d8c06b0a124bc2f5
shikha230 <shikhajatav396@gmail.com>
2026-03-28 17:07:39 +0530

    code

--- routes/frontLineRoutes.js
+++ routes/frontLineRoutes.js
@@ -314,5 +314,5 @@ router.post("/scan-complete/:id", validateToken, scanCompleteAction);
 router.post("/scan-scrap/:id", validateToken, scanScrapAction);
 router.get("/select-parts", validateToken, getSelectParts);
 router.get("/select-products", validateToken, getSelectProducts);
-router.get("/all-employee-timeline", adminValidateToken, allEmployeeTimeLine);
+router.get("/all-employee-timeline", validateToken, allEmployeeTimeLine);
 module.exports = router;
