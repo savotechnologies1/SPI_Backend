f4e9affd8dfe0a3266dde4257d5470831174f3ee
shikha230 <shikhajatav396@gmail.com>
2026-03-29 23:03:35 +0530

    updated cdoe

--- functions/mailer.js
+++ functions/mailer.js
@@ -1,67 +1,3 @@
-// const nodemailer = require("nodemailer");
-// const smtpTransport = require("nodemailer-smtp-transport");
-// const prisma = require("../config/prisma");
-
-// module.exports.sendMail = (templateName, mailVariables, email) => {
-//   return new Promise(async function (resolve, reject) {
-//     try {
-//       const template = await prisma.mailTemplate.findFirst({
-//         where: {
-//           templateEvent: templateName,
-//           isDeleted: false,
-//           active: true,
-//         },
-//       });
-
-//       if (!template) {
-//         return reject(new Error("Mail template not found"));
-//       }
-
-//       let subject = template?.subject;
-//       let html = template?.htmlBody;
-//       let text = template?.textBody;
-//       const transporter = nodemailer.createTransport(
-//         smtpTransport({
-//           pool: true,
-//           host: "smtpout.secureserver.net",
-//           port: 465,
-//           secure: true,
-//           auth: {
-//             user: process.env.SMTP_EMAIL,
-//             pass: process.env.SMTP_PASSWORD,
-//           },
-//         }),
-//       );
-
-//       for (let key in mailVariables) {
-//         subject = subject?.replaceAll(key, mailVariables[key]);
-//         html = html?.replaceAll(key, mailVariables[key]);
-//         text = text?.replaceAll(key, mailVariables[key]);
-//       }
-//       const cleanHtml = html.replace(/\r/g, "").replace(/\n/g, "");
-//       const options = {
-//         from: process.env.SMTP_EMAIL,
-//         to: email,
-//         subject: subject,
-//         text: text,
-//         html: cleanHtml,
-//       };
-
-//       transporter.sendMail(options, function (error) {
-//         if (error) {
-//           return reject(error);
-//         }
-
-//         return resolve({
-//           type: "success",
-//           message: "Mail successfully sent",
-//         });
-//       });
-//     } catch (error) {
-//       return reject(error);
-//     }
-//   });
-// };
 const nodemailer = require("nodemailer");
 const smtpTransport = require("nodemailer-smtp-transport");
 const prisma = require("../config/prisma");
@@ -69,30 +5,31 @@ const prisma = require("../config/prisma");
 module.exports.sendMail = (templateName, mailVariables, email) => {
   return new Promise(async function (resolve, reject) {
     try {
-      const template = await prisma.mailTemplate.findUnique({
+      const template = await prisma.mailTemplate.findFirst({
         where: {
           templateEvent: templateName,
           isDeleted: false,
           active: true,
         },
       });
+
+      if (!template) {
+        return reject(new Error("Mail template not found"));
+      }
+
       let subject = template?.subject;
       let html = template?.htmlBody;
       let text = template?.textBody;
-
       const transporter = nodemailer.createTransport(
         smtpTransport({
           pool: true,
-          host: "smtp.gmail.com",
+          host: "smtpout.secureserver.net",
           port: 465,
-          auth: {
-            user: "shikhajatav23march@gmail.com",
-            pass: "aoal udqz ftky adur",
-          },
           secure: true,
-          // tls: {
-          //   rejectUnauthorized: false,
-          // },
+          auth: {
+            user: process.env.SMTP_EMAIL,
+            pass: process.env.SMTP_PASSWORD,
+          },
         }),
       );
 
@@ -101,12 +38,13 @@ module.exports.sendMail = (templateName, mailVariables, email) => {
         html = html?.replaceAll(key, mailVariables[key]);
         text = text?.replaceAll(key, mailVariables[key]);
       }
+      const cleanHtml = html.replace(/\r/g, "").replace(/\n/g, "");
       const options = {
-        from: "shikhajatav23march@gmail.com",
+        from: process.env.SMTP_EMAIL,
         to: email,
         subject: subject,
         text: text,
-        html: html,
+        html: cleanHtml,
       };
 
       transporter.sendMail(options, function (error) {
@@ -124,3 +62,65 @@ module.exports.sendMail = (templateName, mailVariables, email) => {
     }
   });
 };
+// const nodemailer = require("nodemailer");
+// const smtpTransport = require("nodemailer-smtp-transport");
+// const prisma = require("../config/prisma");
+
+// module.exports.sendMail = (templateName, mailVariables, email) => {
+//   return new Promise(async function (resolve, reject) {
+//     try {
+//       const template = await prisma.mailTemplate.findUnique({
+//         where: {
+//           templateEvent: templateName,
+//           isDeleted: false,
+//           active: true,
+//         },
+//       });
+//       let subject = template?.subject;
+//       let html = template?.htmlBody;
+//       let text = template?.textBody;
+
+//       const transporter = nodemailer.createTransport(
+//         smtpTransport({
+//           pool: true,
+//           host: "smtp.gmail.com",
+//           port: 465,
+//           auth: {
+//             user: "shikhajatav23march@gmail.com",
+//             pass: "aoal udqz ftky adur",
+//           },
+//           secure: true,
+//           // tls: {
+//           //   rejectUnauthorized: false,
+//           // },
+//         }),
+//       );
+
+//       for (let key in mailVariables) {
+//         subject = subject?.replaceAll(key, mailVariables[key]);
+//         html = html?.replaceAll(key, mailVariables[key]);
+//         text = text?.replaceAll(key, mailVariables[key]);
+//       }
+//       const options = {
+//         from: "shikhajatav23march@gmail.com",
+//         to: email,
+//         subject: subject,
+//         text: text,
+//         html: html,
+//       };
+
+//       transporter.sendMail(options, function (error) {
+//         if (error) {
+//           return reject(error);
+//         }
+
+//         return resolve({
+//           type: "success",
+//           message: "Mail successfully sent",
+//         });
+//       });
+//     } catch (error) {
+//       return reject(error);
+//     }
+//   });
+// };
